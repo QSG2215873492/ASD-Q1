876811805c9b4892904924e3cfc3d25d220a5393
QSG2215873492 <51730728+QSG2215873492@users.noreply.github.com>
2019-06-19 18:56:28 +0800

    Initial Commit

---
+++ ASD Q1/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  ASD Q1
+//
+//  Created by s20181105882ASN on 2019/6/19.
+//  Copyright © 2019 s20181105882ASN. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
