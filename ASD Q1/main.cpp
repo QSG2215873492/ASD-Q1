d60b51ec6d53e5f4a0fa026a389ad9c561acea9f
QSG2215873492 <51730728+QSG2215873492@users.noreply.github.com>
2019-06-19 19:57:31 +0800

    QSGQ1
    作业

--- ASD Q1/main.cpp
+++ ASD Q1/main.cpp
@@ -8,8 +8,36 @@
 
 #include <iostream>
 
+using namespace std;
+
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+    int a[8];
+    for (int i=0; i<8; i++)
+    {
+        cin>>a[i];
+    }
+
+    for(int i=0; i<8; i++)
+    {
+        int x;
+        for(int j=0; j<7-i; j++)
+        {
+            if(a[j]>a[j+1])
+            {
+                x=a[j];
+                a[j]=a[j+1];
+                a[j+1]=x;
+            }
+        }
+    }
+    int s=0;
+    int avg;
+    for(int i=1; i<7; i++)
+    {
+        s+=a[i];
+        cout<<a[i]<<endl;
+    }
+    avg=s/6;
+    cout<<avg<<endl;
     return 0;
 }
