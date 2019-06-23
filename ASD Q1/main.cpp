55484d557667d7c6ce355d1a53ed90cf097fa291
QSG2215873492 <51730728+QSG2215873492@users.noreply.github.com>
2019-06-23 17:45:04 +0800

    Update main.cpp

--- ASD Q1/main.cpp
+++ ASD Q1/main.cpp
@@ -1,86 +1,86 @@
-//
-//  main.cpp
-//  ASD Q1
-//
-//  Created by s20181105882ASN on 2019/6/19.
-//  Copyright © 2019 s20181105882ASN. All rights reserved.
-//
 
 #include <iostream>
 #include <fstream>
 #include <string>
-#include <iomainp>
+#include <iomanip>
 
 using namespace std;
+
+ifstream stu("Users/s20181105882ASN/Desktop/选手信息.txt");
+ifstream jud("/Users/s20181105882ASN/Desktop/裁判打分.txt");
+ofstream gra("/Users/s20181105882ASN/Desktop/grade.txt");
 
 class DF
 {
 private:
-    int avg;
-    string number,name,college;
+    double grade[7]={0};
+    string number,name,college,judge[7];
 public:
-    void average();
+    double average;
+    DF(string num,string nam,string col);
     void cout();
 };
 
-void DF::average()
+DF::DF(string num,string nam,string col):number(num),name(nam),college(col)
 {
-    char b[999];
-    int a[8];
-    ifstream A("/Users/s20181105882ASN/Desktop/裁判打分.txt");
-    ofstream C("/Users/s20181105882ASN/Desktop/A.txt");
-    if(A.is_open())
+    double sum=0;
+    for(int i=0; i<7;i++)
     {
-        int x;
-        int sum=0;
-        A.getline(b,999);
-        C<<b<<endl;
-        for(int i=0; i<7; i++)
+        jud>>judge[i];
+        jud>>grade[i];
+        for(int i=0;i<7;i++)
         {
-            A>>a[i];
-        }
-        for(int i=0; i<7; i++)
-        {
-            C<<a[i]<<" ";
-        }
-        
-        for(int i=0; i<7; i++)
-        {
-            for(int j=0; j<6-i; j++)
+            for(int k=0;k<6-i;k++)
             {
-                if(a[j]>a[j+1])
+                if(grade[k]>grade[k+1])
                 {
-                    x=a[j];
-                    a[j]=a[j+1];
-                    a[j+1]=x;
+                    double s=grade[k];
+                    grade[k]=grade[k+1];
+                    grade[k+1]=s;
                 }
             }
         }
-
-        for(int i=1; i<6; i++)
+        for(i=1;i<6;i++)
         {
-            sum +=a[i];
+            sum+=grade[i];
         }
-        avg=sum/5;
-        C<<avg<<endl;
+        average=sum/5.0;
     }
 }
 
 void DF::cout()
 {
-    ofstream C("/Users/s20181105882ASN/Desktop/A.txt");
-    ifstream B("/Users/s20181105882ASN/Desktop/选手信息.txt");
-    char c[999];
-    if(B.is_open())
+    gra<<number <<"  "<<name <<"  "<<college<<endl;
+    for(int i=0;i<7;i++)
     {
-        C<<"OK"<<endl;
-        B.getline(c,999);
-        C<<c<<endl;
+        gra<<judge[i]<<"  "<<grade[i]<<"  ";
     }
+    gra<<"average="<<average<<endl;
 }
+
 int main(int argc, const char * argv[]) {
-    DF m;
-    m.cout();
-    m.average();
+    DF *avg[8];
+    string number,name,college;
+    for(int i=0;i<8;i++)
+    {
+        stu>>number>>name>>college;
+        avg[i]=new DF(number,name,college);
+    }
+    for(int i=0;i<7;i++)
+    {
+        for(int j=0;j<7-i;j++)
+        {
+            if(avg[j]>avg[j+1])
+            {
+                DF *k= avg[j];
+                avg[j]= avg[j+1];
+                avg[j+1]=k;
+            }
+        }
+    }
+    for(int i=0;i<8;i++)
+    {
+        avg[i]->cout();
+    }
     return 0;
 }
