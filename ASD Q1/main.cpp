9c5b592835185d9c08afbfaa4ba5b2ca0cedfb5c
QSG2215873492 <51730728+QSG2215873492@users.noreply.github.com>
2019-06-26 21:17:01 +0800

    endl1

--- ASD Q1/main.cpp
+++ ASD Q1/main.cpp
@@ -9,15 +9,15 @@ ifstream stu("/Users/s20181105882ASN/Desktop/选手信息.txt");
 ifstream jud("/Users/s20181105882ASN/Desktop/裁判打分.txt");
 ofstream gra("/Users/s20181105882ASN/Desktop/grade.txt");
 
-class DF
+class Game
 {
 private:
     double grade[7]={0};
-    
     string number,name,college,judge[7];
 public:
     double average;
-    DF(string num,string nam,string col);
+    double sum=0;
+    Game(string num,string nam,string col);
     void play()
     {
         gra<<number<<"  "<<name<<"  "<<college<<endl;
@@ -26,20 +26,18 @@ public:
             gra<<judge[i]<<" "<<grade[i];
             if(i!=6)
                 gra<<" ";
-            
         }
         gra<<endl;
         gra<<"average="<<average<<endl;
     }
 };
-DF::DF(string num,string nam,string col):number(num),name(nam),college(col)
+
+Game::Game(string num,string nam,string col):number(num),name(nam),college(col)
 {
-    double sum=0;
     for(int i=0;i<7;i++)
     {
         jud>>judge[i];
         jud>>grade[i];
-        
     }
     for(int i=0;i<6;i++)
     {
@@ -53,24 +51,22 @@ DF::DF(string num,string nam,string col):number(num),name(nam),college(col)
             }
         }
     }
-    for(int i=0;i<7;i++)
-        cout<<grade[i]<<" ";
     cout<<endl;
     for(int i=1;i<6;i++)
     {
         sum+=grade[i];
     }
     average=sum/5.0;
-    
 }
+
 int main()
 {
     string name,number,college;
-    DF *avg[8];
+    Game *avg[8];
     for(int i=0;i<8;i++)
     {
         stu>>number>>name>>college;
-        avg[i]=new DF(number,name,college);
+        avg[i]=new Game(number,name,college);
     }
     for(int i=0;i<7;i++)
     {
@@ -78,15 +74,11 @@ int main()
         {
             if(avg[j]->average<avg[j+1]->average)
             {
-                DF *t=avg[j+1];
+                Game *t=avg[j+1];
                 avg[j+1]=avg[j];
                 avg[j]=t;
             }
         }
-    }
-    for(int i=0;i<8;i++)
-    {
-        cout<<avg[i]->average<<" ";
     }
     for(int m=0;m<8;m++)
     {
